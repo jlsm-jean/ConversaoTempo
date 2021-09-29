6aa7b2c86d199cb4c81a4549e69c7e65bde8b613
Jean Leonardo Soares de Moraes <jean.jlsm2@gmail.com>
2021-09-29 12:12:25 -0300

    Add project files.

---
+++ ConversaoTempo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConversaoTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+            var hoursSeconds = 3600;
+            var hours = timeInSeconds/ hoursSeconds;
+            var minutes =  (timeInSeconds - (hoursSeconds * hours)) / 60;
+            var seconds = ((timeInSeconds - (hoursSeconds * hours)) - (minutes * 60));
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+    }
+
